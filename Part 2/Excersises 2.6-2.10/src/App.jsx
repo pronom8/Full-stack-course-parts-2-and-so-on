24f34121e6a4671390970865a34f83ec529e931f
pronom8 <139699276+pronom8@users.noreply.github.com>
2024-06-23 14:23:37 +0300

    2.6-2.10 DONE
    here is the final version of excersises 2.6-2.10

---
+++ Part 2/Excersises 2.6-2.10/src/App.jsx
@@ -0,0 +1,93 @@
+import { useState } from 'react'
+import FilterForm from './filter'
+import PersonForm from './personform'
+import PersonsList from './personlist'
+
+
+
+
+
+
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', id: 1, number: '0403829423' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+
+  const handlePersonChange = (event) => {
+    event.preventDefault()
+    console.log('to be added', event.target)
+    setNewName(event.target.value)
+   
+  }
+
+  const handleNumberChange = (event) => {
+    event.preventDefault()
+    console.log('this is the number to be added:', event.target)
+    setNewNumber(event.target.value)
+
+  }
+
+  const handleFilterChange = (event) => {
+    event.preventDefault()
+    console.log('this is the filter to be added:', event.target)
+    setFilter(event.target.value)
+
+  }
+
+ 
+
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to the phonebook`)
+      return
+    }
+    else {
+      const personObject = {
+        name: newName,
+        number: newNumber,
+        id: persons.length +1
+
+      }
+    
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    setNewNumber('')
+  }}
+
+
+  const peopleToShow = filter
+    ? persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+    : persons
+
+
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      
+      <FilterForm filter={filter} handleFilterChange={handleFilterChange} />
+      
+      <h2>Add a new</h2>
+      
+      <PersonForm
+        newName={newName}
+        newNumber={newNumber}
+        handlePersonChange={handlePersonChange}
+        handleNumberChange={handleNumberChange}
+        addPerson={addPerson}
+      />
+      
+      <PersonsList personsToShow={peopleToShow} />
+    </div>
+  )
+
+}
+
+export default App
